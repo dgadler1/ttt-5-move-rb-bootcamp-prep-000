d6cbc8916da29e122fd3a2c10552a5c96681bdba
Daniel Adler <danad716@gmail.com>
2019-02-02 02:19:54 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,4 +8,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+def input_to_index(input)
+newspot = spot - 1
+end
+
+def move(array, index, value)
+array[index] = value
+end
